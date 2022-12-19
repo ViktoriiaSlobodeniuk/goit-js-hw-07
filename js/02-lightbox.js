fbf27be65673b42634969cd6743e7a8189fa7c33
Viktoriia Slobodeniuk <vikavas988@gmail.com>
2022-12-20 01:21:33 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -12,9 +12,9 @@ const markup = galleryItems
       preview,
       original,
       description,
-    }) => `<a class="gallery__item" href="${original}"> 
+    }) => `<li><a class="gallery__item" href="${original}"> 
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`
+</a></li>`
   )
   .join("");
 galleryEl.innerHTML = markup;
